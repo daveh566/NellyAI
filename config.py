e28879ed2fa81c7ef1749f858ef883d2738eafd0
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-09-26 10:08:31 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+
+API_ID = os.environ.get("API_ID", None) 
+API_HASH = os.environ.get("API_HASH", None) 
+TOKEN = os.environ.get("TOKEN", None) 
+
+
+kuki = Client(
+      "KukiBot",
+      api_id=API_ID,
+      api_hash=API_HASH,
+      bot_token=TOKEN
