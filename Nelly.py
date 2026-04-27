75da77e07d61173f213199327feb75cf0051c78d
Dave KE <69384676+daveh566@users.noreply.github.com>
2026-04-27 16:36:26 +0300

    Fix bot startup syntax and handler issues

--- Nelly.py
+++ Nelly.py
@@ -1,66 +1,61 @@
-print("[INFO]: Importing Your API_ID, API_HASH, BOT_TOKEN")
+import json
+from time import sleep
 
-from pyrogram.types import (
-  Message, 
-)
-from pyrogram import filters, Client 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
-import os
-from config import kuki, BOT_TOKEN
-bot_token= BOT_TOKEN
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-print("[INFO]: Checking... Your Details")
+from config import kuki
+
+print("[INFO]: Checking your details")
+
+
 @kuki.on_message(
     filters.text
     & filters.reply
-    & ~filters.bot
-    & ~filters.edited,
+    & ~filters.bot,
     group=2,
 )
 async def kukiai(client: Client, message: Message):
-  msg = message.text
-  chat_id = message.chat.id
+    msg = message.text
+    chat_id = message.chat.id
 
-  if message.text and not message.document:
-        if not kuki_message(context, message):
-            return
-        Message = message.text
-        kuki.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://kuki-api.tk/api/Raiden/moezilla/message='+Message)
-        Kuki = json.loads(kukiurl.text)
-        kuki = Kuki['reply']
+    if msg and not message.document:
+        await kuki.send_chat_action(chat_id, action="typing")
+        kukiurl = requests.get(
+            f"https://kuki-api.tk/api/Raiden/moezilla/message={msg}",
+            timeout=30,
+        )
+        kuki_json = json.loads(kukiurl.text)
+        reply = kuki_json.get("reply", "I could not generate a response.")
         sleep(0.3)
-        message.reply_text(kuki, timeout=60)
+        await message.reply_text(reply, quote=True)
 
 
-messageprivate = '''
+messageprivate = """
 Hi, I'm Kuki Chat Bot
-'''
+"""
 
-messagegroup = '''
+messagegroup = """
 Hi, I'm Kuki Chat Bot
-'''
-
-
-
-
+"""
 
 
 @kuki.on_message(filters.command("start"))
-async def start(_, message):
-    self = await kuki.get_me()
-    busername = self.username
+async def start(_, message: Message):
+    self_user = await kuki.get_me()
+    _ = self_user.username
     if message.chat.type != "private":
         await message.reply_text(messagegroup)
         return
-    else:
-        buttons = [[InlineKeyboardButton("Github", url="https://github.com/daveh566"),
-                    ]]
-        await message.reply_text(messageprivate, reply_markup=InlineKeyboardMarkup(buttons))
 
-        await kuki.start()
-print(
-        """
-Your Nelly Is Deployed Successfully.
-"""
+    buttons = [[InlineKeyboardButton("Github", url="https://github.com/daveh566")]]
+    await message.reply_text(
+        messageprivate,
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
+
+
+if __name__ == "__main__":
+    print("\nYour Nelly is starting...\n")
+    kuki.run()
